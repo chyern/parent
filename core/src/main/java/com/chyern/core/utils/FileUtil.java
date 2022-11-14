1848ba36a885dd8031b9418240d48e1807399d5d
chyern <chyern@163.com>
2022-11-14 16:42:03 +0800

    添加合并文件方法

--- core/src/main/java/com/chyern/core/utils/FileUtil.java
+++ core/src/main/java/com/chyern/core/utils/FileUtil.java
@@ -6,6 +6,7 @@ import org.apache.commons.lang3.StringUtils;
 import java.io.*;
 import java.nio.file.FileAlreadyExistsException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -46,6 +47,39 @@ public class FileUtil {
     public static void writeFile(String url, List<String> lines) throws Exception {
         String join = StringUtils.join(lines, System.lineSeparator());
         writeFile(url, join);
+    }
+
+    public static File mergeFile(String outFilePath, LinkedList<File> files) throws Exception {
+        File outputFile = new File(outFilePath);
+
+        File outputFileParentFolder = new File(outputFile.getParent());
+        if (!outputFileParentFolder.exists()) {
+            outputFileParentFolder.mkdirs();
+        }
+        if (outputFile.exists()) {
+            outputFile.delete();
+        }
+
+        try (FileOutputStream fileOutputStream = new FileOutputStream(outputFile)) {
+            for (File file : files) {
+                if (!file.exists() || file.isDirectory()) {
+                    continue;
+                }
+                try (FileInputStream fileInputStream = new FileInputStream(file)) {
+                    byte b[] = new byte[10240];
+                    int read = fileInputStream.read(b);
+                    while (read > 0) {
+                        fileOutputStream.write(b, 0, read);
+                        fileOutputStream.flush();
+                        read = fileInputStream.read(b);
+                    }
+                }
+            }
+            fileOutputStream.write(System.lineSeparator().getBytes());
+            fileOutputStream.flush();
+        }
+
+        return outputFile;
     }
 
 }
