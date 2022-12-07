a7f4b1d2b6cdeda711b97553de14fd1d9c113627
chyern <chyern@163.com>
2022-12-07 17:20:00 +0800

    修改版本

--- mysql/src/main/java/com/chyern/mysql/method/InsertBatchMethod.java
+++ mysql/src/main/java/com/chyern/mysql/method/InsertBatchMethod.java
@@ -1,10 +1,17 @@
 package com.chyern.mysql.method;
 
 import com.baomidou.mybatisplus.core.injector.AbstractMethod;
+import com.baomidou.mybatisplus.core.metadata.TableFieldInfo;
 import com.baomidou.mybatisplus.core.metadata.TableInfo;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.ibatis.executor.keygen.NoKeyGenerator;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.mapping.SqlSource;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Description: TODO
@@ -17,30 +24,41 @@ public class InsertBatchMethod extends AbstractMethod {
     @Override
     public MappedStatement injectMappedStatement(Class<?> mapperClass, Class<?> modelClass, TableInfo tableInfo) {
         final String sql = "<script>\nINSERT INTO %s %s VALUES %s\n</script>";
-        final String fieldSql = prepareFieldSql(tableInfo);
-        final String valueSql = prepareValuesSql(tableInfo);
+        List<TableFieldInfo> fieldList = tableInfo.getFieldList();
+
+        LinkedHashMap<String, String> tableFieldMap = getTableFieldMap(tableInfo);
+        final String fieldSql = prepareFieldSql(tableFieldMap);
+        final String valueSql = prepareValuesSql(tableFieldMap);
         final String sqlResult = String.format(sql, tableInfo.getTableName(), fieldSql, valueSql);
         SqlSource sqlSource = languageDriver.createSqlSource(configuration, sqlResult, modelClass);
         return this.addInsertMappedStatement(mapperClass, modelClass, "insertBatch", sqlSource, new NoKeyGenerator(), null, null);
     }
 
-    private String prepareFieldSql(TableInfo tableInfo) {
-        StringBuilder fieldSql = new StringBuilder();
-        fieldSql.append(tableInfo.getKeyColumn()).append(",");
-        tableInfo.getFieldList().forEach(x -> fieldSql.append(x.getColumn()).append(","));
-        fieldSql.delete(fieldSql.length() - 1, fieldSql.length());
-        fieldSql.insert(0, "(");
-        fieldSql.append(")");
-        return fieldSql.toString();
+    private LinkedHashMap<String, String> getTableFieldMap(TableInfo tableInfo) {
+        LinkedHashMap<String, String> tableFieldMap = new LinkedHashMap<>();
+
+        if (StringUtils.isNotBlank(tableInfo.getKeyColumn()) && StringUtils.isNotBlank(tableInfo.getKeyProperty())) {
+            tableFieldMap.put(tableInfo.getKeyColumn(), tableInfo.getKeyProperty());
+        }
+
+        List<TableFieldInfo> fieldList = tableInfo.getFieldList();
+        fieldList.forEach(field -> {
+            tableFieldMap.put(field.getColumn(), field.getProperty());
+        });
+        return tableFieldMap;
     }
 
-    private String prepareValuesSql(TableInfo tableInfo) {
-        final StringBuilder valueSql = new StringBuilder();
-        valueSql.append("<foreach collection=\"list\" item=\"item\" index=\"index\" open=\"(\" separator=\"),(\" close=\")\">");
-        valueSql.append("#{item.").append(tableInfo.getKeyProperty()).append("},");
-        tableInfo.getFieldList().forEach(x -> valueSql.append("#{item.").append(x.getProperty()).append("},"));
-        valueSql.delete(valueSql.length() - 1, valueSql.length());
-        valueSql.append("</foreach>");
-        return valueSql.toString();
+    private String prepareFieldSql(LinkedHashMap<String, String> tableFieldMap) {
+        List<String> fieldColumns = new ArrayList<>(tableFieldMap.keySet());
+        return "(" + StringUtils.join(fieldColumns, ",") + ")";
+    }
+
+    private String prepareValuesSql(LinkedHashMap<String, String> tableFieldMap) {
+        String begin = "<foreach collection=\"list\" item=\"item\" index=\"index\" open=\"(\" separator=\"),(\" close=\")\">";
+        String end = "</foreach>";
+
+        List<String> fieldPropertys = tableFieldMap.values().stream().map(s -> "#{item." + s + "}").collect(Collectors.toList());
+
+        return begin + StringUtils.join(fieldPropertys, ",") + end;
     }
 }
