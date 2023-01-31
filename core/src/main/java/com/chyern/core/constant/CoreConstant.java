7249a722e17aa740da49fda0ac5f9739ac7df800
chyern <chyern@163.com>
2023-01-31 15:15:49 +0800

    优化代码

---
+++ core/src/main/java/com/chyern/core/constant/CoreConstant.java
@@ -0,0 +1,43 @@
+package com.chyern.core.constant;
+
+/**
+ * Description: TODO
+ *
+ * @author Chyern
+ * @since 2023/1/31 15:11
+ */
+public interface CoreConstant {
+
+    /**
+     * 未删除
+     */
+    Integer IS_DELETE_Y = 1;
+
+    /**
+     * 已删除
+     */
+    Integer IS_DELETE_N = 0;
+
+    /**
+     * 启用
+     */
+    Integer IS_ACTIVE_Y = 1;
+
+    /**
+     * 未启用
+     */
+    Integer IS_ACTIVE_N = 0;
+
+    /**
+     * Y
+     */
+    String Y = "Y";
+
+    /**
+     * N
+     */
+    String N = "N";
+
+
+
+}
--- mysql/src/main/java/com/chyern/mysql/domain/BaseEntity.java
+++ mysql/src/main/java/com/chyern/mysql/domain/BaseEntity.java
@@ -3,6 +3,7 @@ package com.chyern.mysql.domain;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.chyern.core.constant.CoreConstant;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -43,5 +44,5 @@ public class BaseEntity implements Serializable {
      * 是否删除(0表示未删除)
      */
     @TableLogic(value = "0", delval = "1")
-    private Integer isDelete = 0;
+    private Integer isDelete = CoreConstant.IS_DELETE_N;
 }
