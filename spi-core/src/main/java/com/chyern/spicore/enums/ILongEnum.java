5956c2bd21e24c95c94da9e973dc4158c98e1671
chyern <chyern@163.com>
2023-03-02 17:32:52 +0800

    优化代码

---
+++ spi-core/src/main/java/com/chyern/spicore/enums/ILongEnum.java
@@ -0,0 +1,14 @@
+package com.chyern.spicore.enums;
+
+/**
+ * Description: TODO
+ *
+ * @author Chyern
+ * @since 2022/7/28 12:02
+ */
+public interface ILongEnum {
+
+    Long getCode();
+
+    String getDesc();
+}
