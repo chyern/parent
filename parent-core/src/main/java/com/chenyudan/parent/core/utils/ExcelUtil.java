6d1d2aaa328bf2fdc355d5c881d3076ee5a473ab
chenyudan <chyern@163.com>
2023-06-08 15:25:18 +0800

    excel处理

--- parent-core/src/main/java/com/chenyudan/parent/core/utils/ExcelUtil.java
+++ parent-core/src/main/java/com/chenyudan/parent/core/utils/ExcelUtil.java
@@ -1,6 +1,9 @@
 package com.chenyudan.parent.core.utils;
 
 import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.context.AnalysisContext;
+import com.alibaba.excel.event.AnalysisEventListener;
+import com.alibaba.excel.read.listener.ReadListener;
 
 import java.io.File;
 import java.util.List;
@@ -23,5 +26,38 @@ public class ExcelUtil {
     public static <T> File generateExcel(String filePath, String sheetName, List<T> data) {
         EasyExcel.write(filePath, data.get(0).getClass()).sheet(sheetName).doWrite(data);
         return new File(filePath);
+    }
+
+    /**
+     * 读取excel
+     *
+     * @param filePath 文件路径
+     * @param clazz    读取对象
+     */
+    public static <T> List<T> readExcel(String filePath, Class<T> clazz) {
+        return readExcel(filePath, clazz, new ExcelListener<>());
+    }
+
+    /**
+     * 读取excel
+     *
+     * @param filePath     文件路径
+     * @param readListener 监听器
+     */
+    public static <T> List<T> readExcel(String filePath, Class<T> clazz, ReadListener<T> readListener) {
+        return EasyExcel.read(filePath, clazz, readListener).sheet().doReadSync();
+    }
+
+
+    public static class ExcelListener<T> extends AnalysisEventListener<T> {
+
+        @Override
+        public void invoke(T data, AnalysisContext analysisContext) {
+        }
+
+        @Override
+        public void doAfterAllAnalysed(AnalysisContext analysisContext) {
+        }
+
     }
 }
--- parent-core/src/test/java/com/chenyudan/parent/core/utils/ExcelUtilTest.java
+++ parent-core/src/test/java/com/chenyudan/parent/core/utils/ExcelUtilTest.java
@@ -6,6 +6,7 @@ import com.alibaba.excel.annotation.format.NumberFormat;
 import com.alibaba.excel.converters.localdatetime.LocalDateTimeDateConverter;
 import com.alibaba.excel.util.FileUtils;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
 import java.time.LocalDateTime;
@@ -19,34 +20,52 @@ import java.util.UUID;
  * @author Chyern
  * @since 2023/6/8 12:34
  */
+@Slf4j
 public class ExcelUtilTest {
 
     public static void main(String[] args) {
-        List<Demo> list = new ArrayList<>();
+        List<WriteBO> writeBOS = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
-            Demo demo = new Demo();
-            demo.setOrderId(UUID.randomUUID().toString());
-            demo.setPayment(0.0D);
-            demo.setCreationTime(LocalDateTime.now());
+            WriteBO writeBO = new WriteBO();
+            writeBO.setOrderId(UUID.randomUUID().toString());
+            writeBO.setPayment(0.0D);
+            writeBO.setCreationTime(LocalDateTime.now());
 
-            list.add(demo);
+            writeBOS.add(writeBO);
         }
 
         String path = FileUtils.getTempFilePrefix() + UUID.randomUUID() + ".xls";
-        File file = ExcelUtil.generateExcel(path, "data", list);
+        File file = ExcelUtil.generateExcel(path, "data", writeBOS);
+
+        log.info(file.getPath());
+
+        List<ReadBO> readBOS = ExcelUtil.readExcel(path, ReadBO.class);
+
         file.delete();
     }
 
     @Data
-    private static class Demo{
+    public static class WriteBO {
         @ExcelProperty("订单编号")
-        private String orderId; // 订单编号
+        private String orderId;
 
         @ExcelProperty("支付金额")
         @NumberFormat("￥#,###")
-        private Double payment; // 支付金额
+        private Double payment;
 
-        @ExcelProperty(value = "创建日期",converter = LocalDateTimeDateConverter.class)
-        private LocalDateTime creationTime; // 创建时间
+        @ExcelProperty(value = "创建日期", converter = LocalDateTimeDateConverter.class)
+        private LocalDateTime creationTime;
+    }
+
+    @Data
+    public static class ReadBO {
+        @ExcelProperty(index = 0)
+        private String orderId;
+
+        @ExcelProperty(index = 1)
+        private String payment;
+
+        @ExcelProperty(index = 2)
+        private String creationTime;
     }
 }
