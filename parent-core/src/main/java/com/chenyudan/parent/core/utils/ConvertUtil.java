f4204abe258737de400c721763d4eb33655f0113
chenyudan <chyern@163.com>
2023-06-30 11:04:36 +0800

    add new peculiarity

---
+++ parent-core/src/main/java/com/chenyudan/parent/core/utils/ConvertUtil.java
@@ -0,0 +1,24 @@
+package com.chenyudan.parent.core.utils;
+
+import java.math.BigDecimal;
+
+/**
+ * Description: TODO
+ *
+ * @author Chyern
+ * @since 2023/6/30 11:00
+ */
+public class ConvertUtil {
+
+    public static double convertDouble(BigDecimal bigDecimal) {
+        return bigDecimal == null ? 0 : bigDecimal.doubleValue();
+    }
+
+    public static boolean convertBoolean(String str) {
+        return Boolean.getBoolean(str);
+    }
+
+    public static int convertInt(Long l) {
+        return l == null ? 0 : l.intValue();
+    }
+}
