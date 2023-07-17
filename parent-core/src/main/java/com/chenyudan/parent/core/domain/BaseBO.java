1ce282b4af63f65258044dd79276ff418ae35e71
chenyudan <chyern@163.com>
2023-07-17 11:31:30 +0800

    add new peculiarity

--- parent-core/src/main/java/com/chenyudan/parent/core/domain/BaseBO.java
+++ parent-core/src/main/java/com/chenyudan/parent/core/domain/BaseBO.java
@@ -2,6 +2,9 @@ package com.chenyudan.parent.core.domain;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+
 /**
  * Description: 基础DTO
  *
@@ -9,10 +12,13 @@ import lombok.Data;
  * @since 2022/12/13 14:17
  */
 @Data
-public class BaseBO {
+public class BaseBO implements Serializable {
+
+    private static final long serialVersionUID = 8028322322166124941L;
 
     /**
      * 组织ID
      */
+    @NotBlank
     private String organizationId;
 }
