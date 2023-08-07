ea0c5a0b4134b8137f53cb1fa283cf503882aa35
chenyudan <chyern@163.com>
2023-08-07 14:31:06 +0800

    add new peculiarity

---
+++ parent-core/src/main/java/com/chenyudan/parent/core/utils/StreamUtil.java
@@ -0,0 +1,13 @@
+package com.chenyudan.parent.core.utils;
+
+import org.springframework.util.StreamUtils;
+
+/**
+ * Description: TODO
+ *
+ * @author Chyern
+ * @since 2023/8/7 14:29
+ */
+public class StreamUtil extends StreamUtils {
+
+}
