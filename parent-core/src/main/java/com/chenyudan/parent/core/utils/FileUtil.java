e2b9b9db4c4595c43d41767767004bd26de52f6a
chenyu <yu.chen@narwal.com>
2024-01-05 10:50:20 +0800

    change

--- parent-core/src/main/java/com/chenyudan/parent/core/utils/FileUtil.java
+++ parent-core/src/main/java/com/chenyudan/parent/core/utils/FileUtil.java
@@ -111,11 +111,7 @@ public class FileUtil {
     public static byte[] readFile(File file) throws IOException {
         AssertUtil.isTrue(file.exists(), BaseExceptionEnum.FILE_NOT_FIND);
         try (FileInputStream fileInputStream = new FileInputStream(file)) {
-//            return fileInputStream.readAllBytes();
-
-            byte[] result = new byte[fileInputStream.available()];
-            fileInputStream.read(result);
-            return result;
+            return fileInputStream.readAllBytes();
         }
     }
 
--- parent-database/src/main/java/com/chenyudan/parent/database/mybatis/interceptor/DefaultInterceptor.java
+++ parent-database/src/main/java/com/chenyudan/parent/database/mybatis/interceptor/DefaultInterceptor.java
@@ -4,7 +4,6 @@ import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -18,7 +17,7 @@ public class DefaultInterceptor implements Interceptor {
     @Override
     public org.apache.ibatis.plugin.Interceptor[] resolveInterceptor() {
         MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
-        mybatisPlusInterceptor.setInterceptors(Arrays.asList(new PaginationInnerInterceptor(DbType.MYSQL)));
+        mybatisPlusInterceptor.setInterceptors(List.of(new PaginationInnerInterceptor(DbType.MYSQL)));
         return new org.apache.ibatis.plugin.Interceptor[]{mybatisPlusInterceptor};
     }
 }
